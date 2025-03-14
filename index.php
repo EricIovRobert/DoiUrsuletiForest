903024a2f063779ebb15c7b61e3ccff732f6199d
ericiov <162496227+EricIovRobert@users.noreply.github.com>
2025-03-14 20:11:00 +0200

    testimoniale

--- index.php
+++ index.php
@@ -361,40 +361,49 @@ session_start(); // Mută session_start() aici, la început
 
 
     <!-- Testimonial Start -->
-    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container">
-            <div class="section-title text-center">
-                <h1 class="display-5 mb-5">Testimonial</h1>
+<div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="display-5 mb-5">Testimoniale</h1>
+        </div>
+        <div class="row g-4">
+            <!-- Testimonial 1 -->
+            <div class="col-md-4">
+                <div class="card h-100 text-center p-4 shadow-sm">
+                    <img class="img-fluid bg-light p-2 mx-auto mb-3 rounded-circle" src="img/testimonial-1.jpg" style="width: 90px; height: 90px" alt="Client 1">
+                    <div class="card-body">
+                        <p class="mb-3">Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr.</p>
+                        <h5 class="mb-1">Ion Popescu</h5>
+                        <span class="fst-italic text-muted">Client satisfăcut</span>
+                    </div>
+                </div>
             </div>
-            <div class="owl-carousel testimonial-carousel">
-                <div class="testimonial-item text-center">
-                    <img class="img-fluid bg-light p-2 mx-auto mb-3" src="img/testimonial-1.jpg" style="width: 90px; height: 90px;">
-                    <div class="testimonial-text text-center p-4">
-                        <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                        <h5 class="mb-1">Client Name</h5>
-                        <span class="fst-italic">Profession</span>
+            <!-- Testimonial 2 -->
+            <div class="col-md-4">
+                <div class="card h-100 text-center p-4 shadow-sm">
+                    <img class="img-fluid bg-light p-2 mx-auto mb-3 rounded-circle" src="img/testimonial-2.jpg" style="width: 90px; height: 90px" alt="Client 2">
+                    <div class="card-body">
+                        <p class="mb-3">Serviciile sunt excelente, recomand cu încredere! Echipa a fost foarte profesionistă și atentă la detalii.</p>
+                        <h5 class="mb-1">Maria Ionescu</h5>
+                        <span class="fst-italic text-muted">Designer interior</span>
                     </div>
                 </div>
-                <div class="testimonial-item text-center">
-                    <img class="img-fluid bg-light p-2 mx-auto mb-3" src="img/testimonial-2.jpg" style="width: 90px; height: 90px;">
-                    <div class="testimonial-text text-center p-4">
-                        <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                        <h5 class="mb-1">Client Name</h5>
-                        <span class="fst-italic">Profession</span>
-                    </div>
-                </div>
-                <div class="testimonial-item text-center">
-                    <img class="img-fluid bg-light p-2 mx-auto mb-3" src="img/testimonial-3.jpg" style="width: 90px; height: 90px;">
-                    <div class="testimonial-text text-center p-4">
-                        <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                        <h5 class="mb-1">Client Name</h5>
-                        <span class="fst-italic">Profession</span>
+            </div>
+            <!-- Testimonial 3 -->
+            <div class="col-md-4">
+                <div class="card h-100 text-center p-4 shadow-sm">
+                    <img class="img-fluid bg-light p-2 mx-auto mb-3 rounded-circle" src="img/testimonial-3.jpg" style="width: 90px; height: 90px" alt="Client 3">
+                    <div class="card-body">
+                        <p class="mb-3">Calitate superioară și livrare la timp. A fost o experiență plăcută să lucrez cu ei!</p>
+                        <h5 class="mb-1">Andrei Popa</h5>
+                        <span class="fst-italic text-muted">Antreprenor</span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Testimonial End -->
+</div>
+<!-- Testimonial End -->
 
 
     <!-- Footer Start -->
